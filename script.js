0f12632947cdea130c75f51b63d82b076898e27a
Gabriel Ejiro <115194289+gabriel-ejiro@users.noreply.github.com>
2024-03-11 18:40:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -93,5 +93,43 @@ tileGroup.selectAll(".tile")
     .attr("width", d => (d.x1 - d.x0))
     .attr("height", d => (d.y1 - d.y0));
 
-// user story 
+// user story #10 & #11
+
+// Append a tooltip element to the body
+const tooltip = d3.select("body")
+  .append("div")
+  .attr("id", "tooltip")
+  .style("opacity", 0); // Initially hide the tooltip
+
+// Create tiles for each data point
+svg.selectAll(".tile")
+  .data(data)
+  .enter()
+  .append("rect")
+  .attr("class", "tile")
+  .attr("x", d => d.x0)
+  .attr("y", d => d.y0)
+  .attr("width", d => d.x1 - d.x0)
+  .attr("height", d => d.y1 - d.y0)
+  .style("fill", d => colorScale(d.category)) // Fill color based on category
+  .attr("data-name", d => d.name) // Add data-name property
+  .attr("data-category", d => d.category) // Add data-category property
+  .attr("data-value", d => d.value) // Add data-value property
+  .on("mouseover", (event, d) => {
+    // Show tooltip on mouseover
+    tooltip.transition()
+      .duration(200)
+      .style("opacity", 0.9);
+    // Set tooltip content
+    tooltip.html(`Name: ${d.name}<br>Category: ${d.category}<br>Value: ${d.value}`)
+      .attr("data-value", d.value) // Set data-value property
+      .style("left", (event.pageX + 10) + "px")
+      .style("top", (event.pageY - 28) + "px");
+  })
+  .on("mouseout", () => {
+    // Hide tooltip on mouseout
+    tooltip.transition()
+      .duration(500)
+      .style("opacity", 0);
+  });
 
